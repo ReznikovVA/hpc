02a2412fef89041f788cb368da5eaaa3272be220
ReznikovVA <149011748+ReznikovVA@users.noreply.github.com>
2024-03-26 10:23:20 +0300

    Update 3_2.cpp

--- 3_2/3_2.cpp
+++ 3_2/3_2.cpp
@@ -28,32 +28,35 @@ void sortVectorInRange(std::vector<int>& vec, size_t start, size_t end) {
 
 int main() {
     std::vector<int> vec = generateRandomVector(VECTOR_SIZE);
-
+    std::vector<int> vec_copy = vec;
+    std::vector<int> vec_copy2 = vec;
+    
     //Однопоточный сорт
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::steady_clock::now();
     std::sort(vec.begin(), vec.end());
-    auto end = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_single = end - start;
     std::cout << "Single-threaded sort took " << duration_single.count() << " seconds." << std::endl;
 
     //Многопоточный сорт
     std::vector<std::thread> threads;
-    start = std::chrono::high_resolution_clock::now();
+    start = std::chrono::steady_clock::now();
     size_t chunkSize = VECTOR_SIZE / NUM_THREADS;
     for (size_t i = 0; i < NUM_THREADS; ++i) {
-        threads.emplace_back(sortVectorInRange, std::ref(vec), i * chunkSize, (i + 1) * chunkSize);
+        threads.emplace_back(sortVectorInRange, std::ref(vec_copy), i * chunkSize, (i + 1) * chunkSize);
     }
     for (auto& thread : threads) {
         thread.join();
     }
-    end = std::chrono::high_resolution_clock::now();
+    std::sort(vec_copy.begin(), vec_copy.end());
+    end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_multi = end - start;
     std::cout << "Multi-threaded sort took " << duration_multi.count() << " seconds." << std::endl;
 
     //Паралелльная сортировка через std::execution::par
     start = std::chrono::high_resolution_clock::now();
-    std::sort(std::execution::par, vec.begin(), vec.end());
-    end = std::chrono::high_resolution_clock::now();
+    std::sort(std::execution::par, vec_copy2.begin(), vec_copy2.end());
+    end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_parallel = end - start;
     std::cout << "Parallel std::execution sort took " << duration_parallel.count() << " seconds." << std::endl;
 
