e9888921ddb5af4bd2ac4f8934be3a3b4ba2edeb
ReznikovVA <149011748+ReznikovVA@users.noreply.github.com>
2024-03-28 23:02:56 +0300

    Update 3_2.cpp

--- 3_2/3_2.cpp
+++ 3_2/3_2.cpp
@@ -26,19 +26,29 @@ void sortVectorInRange(std::vector<int>& vec, size_t start, size_t end) {
     std::sort(vec.begin() + start, vec.begin() + end);
 }
 
+void check_sorted(const std::vector<int>& v) {
+    for (size_t i = 1; i < v.size(); i++) {
+        if (v[i] < v[i-1]) {
+            std::cerr << "Wrong order in position " << i - 1 << ": " << v[i-1] << ", " << v[i] << std::endl; 
+            exit(EXIT_FAILURE);
+        }
+    }   
+}
+
 int main() {
     std::vector<int> vec = generateRandomVector(VECTOR_SIZE);
     std::vector<int> vec_copy = vec;
     std::vector<int> vec_copy2 = vec;
     
-    //Однопоточный сорт
+    // Однопоточный сорт
     auto start = std::chrono::steady_clock::now();
     std::sort(vec.begin(), vec.end());
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_single = end - start;
     std::cout << "Single-threaded sort took " << duration_single.count() << " seconds." << std::endl;
+    check_sorted(vec);
 
-    //Многопоточный сорт
+    // Многопоточный сорт
     std::vector<std::thread> threads;
     start = std::chrono::steady_clock::now();
     size_t chunkSize = VECTOR_SIZE / NUM_THREADS;
@@ -48,17 +58,22 @@ int main() {
     for (auto& thread : threads) {
         thread.join();
     }
-    std::sort(vec_copy.begin(), vec_copy.end());
+    
+    for (size_t i = 1; i < NUM_THREADS; ++i) {
+        std::inplace_merge(vec_copy.begin(), vec_copy.begin() + i * chunkSize, vec_copy.begin() + (i + 1) * chunkSize);
+    }
     end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_multi = end - start;
     std::cout << "Multi-threaded sort took " << duration_multi.count() << " seconds." << std::endl;
+    check_sorted(vec_copy);
 
-    //Паралелльная сортировка через std::execution::par
-    start = std::chrono::high_resolution_clock::now();
+    // Параллельная сортировка через std::execution::par
+    start = std::chrono::steady_clock::now();
     std::sort(std::execution::par, vec_copy2.begin(), vec_copy2.end());
     end = std::chrono::steady_clock::now();
     std::chrono::duration<double> duration_parallel = end - start;
     std::cout << "Parallel std::execution sort took " << duration_parallel.count() << " seconds." << std::endl;
+    check_sorted(vec_copy2);
 
     return 0;
 }
